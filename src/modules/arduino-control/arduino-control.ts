577b330c78bbe864ff85fe57995433bd3cdf6e6a
Benjamin Schmitz <vortex@wolpzone.de>
2018-02-07 23:32:18 +0100

    add log message

--- src/modules/arduino-control/arduino-control.ts
+++ src/modules/arduino-control/arduino-control.ts
@@ -154,7 +154,7 @@ export class ArduinoControl extends Service {
         }
         /* assume repeat */
         let offset = Buffer.from(msg).readUInt16LE(0)
-        log.debug({len: msg.byteLength}, "write LONG_PAYLOAD")
+        log.debug({len: msg.byteLength, offset: offset}, "write LONG_PAYLOAD")
         this.buf = longPayloadHeader(this.memberAddress, msg.byteLength, offset, true)
         this.longPayload = Buffer.from(msg, 2)
 
